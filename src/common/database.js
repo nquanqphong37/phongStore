3197bc6e547244f5dce6a9bd6e34e4ac39e7cfb3
nquanqphong37 <156775783+nquanqphong37@users.noreply.github.com>
2024-05-25 09:51:57 +0700

    Update database.js

--- src/common/database.js
+++ src/common/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 module.exports = () => {
   mongoose
-    .connect("mongodb+srv://bigcityboi:wolp2EYG7mFxhOFN@cluster0.cuicfuc.mongodb.net/test?retryWrites=true&w=majority")
+    .connect("mongodb+srv://bigcityboi:ironlauren@cluster0.cuicfuc.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0")
     .then(() => console.log("Connected!"));
   return mongoose;
 };
